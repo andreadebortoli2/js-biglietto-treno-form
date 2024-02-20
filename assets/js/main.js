c84e604ee4382c69ffeaa5382298d49fe40a2f59
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-02-20 16:22:44 +0100

    add console.log on btn click

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,23 @@
 
 //         -user name
 const userName = document.getElementById(`user_name`).value;
-console.log(userName);
+// console.log(userName);
 
 //         -travel length
 const travelLength = document.getElementById(`travel_length`).value;
-console.log(travelLength);
+// console.log(travelLength);
 
 //         -user age
 const userAge = document.getElementById(`user_age`).value;
-console.log(userAge);
+// console.log(userAge);
+
+//-show variable in console at the click
+
+const submitBtn = document.querySelector(`button`)
+
+submitBtn.addEventListener(`click`, function() {
+    console.log(userName, travelLength, userAge);
+})
 
 //     -calculate travel price
 
