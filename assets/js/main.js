3c2de6407999b4c66ed7880a596810226ab43900
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-02-20 16:14:02 +0100

    add user variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 // console.log(`Hi`);
+
+// -ask and set as variables:
+
+//         -user name
+const userName = document.getElementById(`user_name`).value;
+console.log(userName);
+
+//         -travel length
+const travelLength = document.getElementById(`travel_length`).value;
+console.log(travelLength);
+
+//         -user age
+const userAge = document.getElementById(`user_age`).value;
+console.log(userAge);
+
+//     -calculate travel price
+
+
+
+//     -apply discounts
+//     -show results on console.log()
